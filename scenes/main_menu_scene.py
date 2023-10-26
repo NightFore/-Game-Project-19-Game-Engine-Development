0e0dbdaa6d69b3a15a37139ce2e8525735f70a75
NightFore <Et3rnNighTaLL@Hotmail.com>
2023-10-26 23:08:44 +0200

    Version 0.3.5c: Scene Integration - TemplateScene (Scene Management, Scene Logic, Custom Functions)

--- scenes/main_menu_scene.py
+++ scenes/main_menu_scene.py
@@ -4,43 +4,47 @@ class MainMenuScene(SceneBase):
     def __init__(self):
         super().__init__()
 
-    def debug_audio_manager(self):
-        self.audio_manager.play_music("debug_music")
-        self.audio_manager.play_sound("debug_sound")
-
+    """
+    Scene Management
+    """
     def enter(self):
         super().enter()
-        self.audio_manager = self.managers["audio_manager"]
-        self.graphic_manager = self.managers["graphic_manager"]
-
-        # Create a single image graphic instance
-        self.single_graphic = self.graphic_manager.create_graphic_instance("default_single", "image")
-
-        # Create an image sequence graphic instance
-        self.sequence_animation = self.graphic_manager.create_graphic_instance("default_sequence", "image_sequence")
+        self.init_managers()
+        self.init_graphics()
 
     def exit(self):
         super().exit()
 
     def update(self, dt):
         super().update(dt)
-
         self.update_buttons()
         self.update_graphics(dt)
+
+    def draw(self, screen):
+        super().draw(screen)
+        self.draw_graphics(screen)
+
+
+    """
+    Scene Logic
+    """
+    def init_managers(self):
+        self.audio_manager = self.managers["audio_manager"]
+        self.graphic_manager = self.managers["graphic_manager"]
+
+    def init_graphics(self):
+        self.single_graphic = self.graphic_manager.create_graphic_instance("default_single", "image")
+        self.sequence_animation = self.graphic_manager.create_graphic_instance("default_sequence", "image_sequence")
 
     def update_buttons(self):
         if self.scene_buttons["start"].clicked_and_released:
             self.scene_manager.set_scene("GameScene")
-
         if self.scene_buttons["settings"].clicked_and_released:
             self.scene_manager.set_scene("SettingsScene")
-
         if self.scene_buttons["debug_audio"].clicked_and_released:
             self.debug_audio_manager()
-
         if self.scene_buttons["pause_music"].clicked_and_released:
             self.audio_manager.pause_music()
-
         if self.scene_buttons["toggle_music"].clicked_and_released:
             self.audio_manager.toggle_music()
 
@@ -48,11 +52,14 @@ class MainMenuScene(SceneBase):
         self.single_graphic.update(dt)
         self.sequence_animation.update(dt)
 
-    def draw(self, screen):
-        super().draw(screen)
-
-        # Draw the single graphic
+    def draw_graphics(self, screen):
         self.single_graphic.draw(screen, (100, 100))
-
-        # Draw the sequence animation
         self.sequence_animation.draw(screen, (300, 100))
+
+
+    """
+    Custom Functions
+    """
+    def debug_audio_manager(self):
+        self.audio_manager.play_music("debug_music")
+        self.audio_manager.play_sound("debug_sound")
--- scenes/template.py
+++ scenes/template.py
@@ -1,18 +1,52 @@
-import pygame
 from manager.scene_manager import SceneBase
 
 class TemplateScene(SceneBase):
     def __init__(self):
         super().__init__()
 
+    """
+    Scene Management
+    """
     def enter(self):
         super().enter()
+        self.init_managers()
+        self.init_graphics()
 
     def exit(self):
         super().exit()
 
     def update(self, dt):
         super().update(dt)
+        self.update_buttons()
+        self.update_graphics(dt)
 
     def draw(self, screen):
         super().draw(screen)
+        self.draw_graphics(screen)
+
+
+    """
+    Scene Logic
+    """
+    def init_managers(self):
+        pass
+
+    def init_graphics(self):
+        pass
+
+    def update_buttons(self):
+        pass
+
+    def update_graphics(self, dt):
+        pass
+
+    def draw_graphics(self, screen):
+        pass
+
+
+    """
+    Custom Functions
+    """
+    @staticmethod
+    def hello_world():
+        print("Hello World")
